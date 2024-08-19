8a1220063c8dfe88b59101de9e0ec58009fca421
Asad <mohammad.asad.rao@gmail.com>
2024-08-19 17:02:10 +0000

    Adding CodeDeploy files

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+sudo yum install tomcat -y
+sudo yum -y install httpd
+
+# Use sudo to ensure the heredoc content is written with appropriate permissions
+sudo bash -c 'cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
+<VirtualHost *:80>
+    ServerAdmin root@localhost
+    ServerName app.wildrydes.com
+    DefaultType text/html
+    ProxyRequests off
+    ProxyPreserveHost On
+    ProxyPass / http://localhost:8080/unicorn-web-project/
+    ProxyPassReverse / http://localhost:8080/unicorn-web-project/
+</VirtualHost>
+EOF'
+
